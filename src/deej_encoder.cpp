90be8e5bad140b32bdc0036449fe21905d1e7037
numanair <numanair@gmail.com>
2021-02-27 17:18:44 -0800

    cleanup, pin changes

--- src/deej_encoder.cpp
+++ src/deej_encoder.cpp
@@ -6,7 +6,7 @@
 // Number of LEDs (generally same as NUM_SLIDERS):
 #define NUM_LEDS 1
 // LED control pin:
-#define DATA_PIN 35
+#define DATA_PIN 27
 #define ledBrightness 50
 #define minHue 14
 #define maxHue 102
@@ -42,7 +42,7 @@ void setup() {
 
     ESP32Encoder::useInternalWeakPullResistors = UP;
     // set encoder pins A & B (CLK & DT) here:
-    encoder[0].attachHalfQuad(15, 16);
+    encoder[0].attachHalfQuad(12, 14);
     // encoder[0].attachHalfQuad(15, 16);
     // encoder[1].attachHalfQuad(17, 18);
     // encoder[2].attachHalfQuad(25, 26);
@@ -142,80 +142,4 @@ void checkEncoders() {
             // we aren't actually muted
         }
     }
-
-    // // Discord (encoder2)
-    // if (Discord > 0 && Discord < 102 && Mute[1] == 0) {
-    //     analogSliderValues[1] = Discord * 10;
-    //     int encoder2toLedHue = map(Discord, 0, 102, minHue, maxHue);
-    //     leds[1].setHue(encoder2toLedHue);
-    // } else if (Mute[1] == 0 && (Discord > 102 || Discord == 102)) {
-    //     analogSliderValues[1] = 102 * 10;
-    //     encoder2.setCount(102);
-    //     leds[1].setHue(maxHue);
-    // } else if (Mute[1] == 1) {
-    //     analogSliderValues[1] = 0;
-    //     leds[1] = CRGB::mutecolor;
-    // } else {
-    //     analogSliderValues[1] = 0;
-    //     encoder2.setCount(0);
-    //     leds[1] = CRGB::mutecolor;
-    // }
-    // // End of Discord (encoder2)
-
-    // // Chrome (encoder3)
-    // if (Chrome > 0 && Chrome < 102 && Mute[2] == 0) {
-    //     analogSliderValues[2] = Chrome * 10;
-    //     int encoder3toLedHue = map(Chrome, 0, 102, minHue, maxHue);
-    //     leds[2].setHue(encoder3toLedHue);
-    // } else if (Mute[2] == 0 && (Chrome > 102 || Chrome == 102)) {
-    //     analogSliderValues[2] = 102 * 10;
-    //     encoder3.setCount(102);
-    //     leds[2].setHue(maxHue);
-    // } else if (Mute[2] == 1) {
-    //     analogSliderValues[2] = 0;
-    //     leds[2] = CRGB::mutecolor;
-    // } else {
-    //     analogSliderValues[2] = 0;
-    //     encoder3.setCount(0);
-    //     leds[2] = CRGB::mutecolor;
-    // }
-    // // End of Chrome (encoder3)
-
-    // // Gaming (encoder4)
-    // if (Gaming > 0 && Gaming < 102 && Mute[3] == 0) {
-    //     analogSliderValues[3] = Gaming * 10;
-    //     int encoder4toLedHue = map(Gaming, 0, 102, minHue, maxHue);
-    //     leds[3].setHue(encoder4toLedHue);
-    // } else if (Mute[3] == 0 && (Gaming > 102 || Gaming == 102)) {
-    //     analogSliderValues[3] = 102 * 10;
-    //     encoder4.setCount(102);
-    //     leds[3].setHue(maxHue);
-    // } else if (Mute[3] == 1) {
-    //     analogSliderValues[3] = 0;
-    //     leds[3] = CRGB::mutecolor;
-    // } else {
-    //     analogSliderValues[3] = 0;
-    //     encoder4.setCount(0);
-    //     leds[3] = CRGB::mutecolor;
-    // }
-    // // End of Gaming (encoder4)
-
-    // // Music (encoder5)
-    // if (Music > 0 && Music < 102 && Mute[4] == 0) {
-    //     analogSliderValues[4] = Music * 10;
-    //     int encoder5toLedHue = map(Music, 0, 102, minHue, maxHue);
-    //     leds[4].setHue(encoder5toLedHue);
-    // } else if (Mute[4] == 0 && (Music > 102 || Music == 102)) {
-    //     analogSliderValues[4] = 102 * 10;
-    //     encoder5.setCount(102);
-    //     leds[4].setHue(maxHue);
-    // } else if (Mute[4] == 1) {
-    //     analogSliderValues[4] = 0;
-    //     leds[4] = CRGB::mutecolor;
-    // } else {
-    //     analogSliderValues[4] = 0;
-    //     encoder5.setCount(0);
-    //     leds[4] = CRGB::mutecolor;
-    // }
-    // // End of Music (encoder5)
 }
